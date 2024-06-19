a2d0daad82b534aa9f3b4bd96cdfe5e21ab05539
bryan zack <bryan.zack@zacklabs.com>
2024-06-19 00:38:24 -0400

    removed drag/drop functionality, added dialog on file selection

---
+++ src/app/components/Video.tsx
@@ -0,0 +1,8 @@
+import { DropzoneInputProps, DropzoneRootProps } from "react-dropzone";
+export default function Video() {
+    return (
+       <div className='border border-black rounded w-[400px] h-[350px]'>
+
+       </div> 
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,34 +1,35 @@
 'use client';
-import { useRef } from "react";
-import Image from "next/image";
-import "@/app/globals.css"
-import { useSession, signIn, signOut } from "next-auth/react"
-import { useState } from 'react'
-import VideoUpload from "./components/VideoUploader";
+import "@/app/globals.css";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { useCallback, useState } from 'react';
+import { useFilePicker } from 'use-file-picker';
+import Videos from "./components/Video";
+import CustomizedDialogs from "./components/Dialog";
+import { useAtom } from "jotai";
+import { dialogAtom } from "./state/atoms";
 
 export default function Home() {
-
+  const [dialog, setDialog] = useAtom(dialogAtom);
   const { data: session } = useSession();
-  const [tab, setTab] = useState('videos')
-  const fileInputRef = useRef<HTMLInputElement>(null);
-  const handleButtonClick = () => {
-    if (fileInputRef.current) {
-      fileInputRef.current.click()
-    }
-  }
+  const [tab, setTab] = useState('videos') 
+  const { openFilePicker, filesContent, plainFiles, loading } = useFilePicker({
+    accept: '.mp4',
+    onFilesSuccessfullySelected: ({ filesContent, plainFiles}: {filesContent: any, plainFiles: File[]}) => {
+      console.log('onFilesSucess: ', plainFiles)     ;
+    },
+    onFilesSelected: () => {
+      setDialog(true);
+    },
+    onFilesRejected: ({ errors }) => {
 
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files.length > 0) {
-      const file = e.target.files[0];
-      console.log('Selected file:', file);
-      // Perform your file handling logic here
     }
-  };
+  });
   
   if (session) {
     return (
       <>
-        <div className='w-screen h-screen flex items-center justify-center'>
+        {dialog && <CustomizedDialogs files={plainFiles} /> }
+        <div className={`w-screen h-screen flex items-center justify-center`}>
           <div className='w-[1000px] h-[750px] flex flex-col'>
             <div className='w-full h-[5%] border-b border-black flex items-center'>
             </div>
@@ -40,17 +41,19 @@ export default function Home() {
                     <h2 onClick={() => setTab('friends')} className='px-2 cursor-pointer'>friends</h2>
                     <h2 onClick={() => setTab('activity')} className='px-2 cursor-pointer'>activity</h2>
                   </div> 
-                  <VideoUpload />
+                  <div className='mx-2 px-2 cursor-pointer hover:underline'
+                    onClick={() => openFilePicker()}>upload</div>
                 </div>
               </div>
-              <p>{tab}</p>
+              {tab === 'videos' && <p>Videos</p>}
+              {tab === 'friends' && <p>Friends</p>}
+              {tab === 'activity' && <p>Activity</p>}
             </div>
             <div className="w-full h-[5%] border-t border-black bg-black flex flex-row items-center justify-between text-white px-3">
               <h2>{session.user?.name}</h2>
-              <h2 onClick={() => signOut()} className={'cursor-pointer'}>logout</h2>
+              <h2 onClick={() => signOut()} className={'px-2 hover:underline cursor-pointer'}>logout</h2>
             </div>
           </div>
-
         </div>
       </>
     )
